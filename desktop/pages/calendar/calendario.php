6fe076b54f4c1ffe4ea225aa1107ec4b28dca6d4
AbsintoMod <91625082+AbsintoMod@users.noreply.github.com>
2022-04-26 03:56:32 -0300

    edit calendar

--- desktop/pages/calendar/calendario.php
+++ desktop/pages/calendar/calendario.php
@@ -5,7 +5,7 @@ if (empty($_SESSION['id'])) {
   $_SESSION['msg'] = "<div class='alert alert-danger'>Área restrita!</div>";
   header("Location:../../../login/home.php");
 }
-  require_once '../../../assets/lang/pt-br.php';
+require_once '../../../assets/lang/pt-br.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,13 +15,25 @@ if (empty($_SESSION['id'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php
   //Google Font: Source Sans Pro
-    require_once '../../../assets/head/font_google.php';
-    
+  require_once '../../../assets/head/font_google.php';
+
   // Font Awesome Icons 
-    require_once '../../../assets/head/font_awesome.php';
+  require_once '../../../assets/head/font_awesome.php';
   ?>
   <!-- Theme style -->
   <link rel="stylesheet" href="../../../dist/css/adminlte.min.css">
+  <!-- fullCalendar-->
+  <link rel="stylesheet" href="../../../plugins/fullcalendar/main.css">
+
+  <style>
+    #select-lang {
+      display: none;
+    }
+
+    .fc-toolbar-title{
+      text-transform: uppercase;
+    } 
+  </style>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -31,22 +43,145 @@ if (empty($_SESSION['id'])) {
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-12">
-            <h1 class="m-0"><?=$menu_calendar?></h1>
+            <h1 class="m-0"><?= $menu_calendar ?></h1>
           </div>
         </div>
       </div>
     </div><!-- /.content-header -->
     <!-- Conteudo -->
-
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div id="select-lang" class="col-md-3">
+            Locals:
+            <select id='locale-selector'></select>
+          </div>
+          <!-- /.col -->
+          <div class="col-md-12">
+            <div class="card card-primary">
+              <div class="card-body p-0">
+                <!-- THE CALENDAR -->
+                <div id="calendar"></div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
     <!-- /.Conteudo -->
   </div><!-- /.content -->
 
   <!-- jQuery -->
   <script src="../../../plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
+  <!-- Bootstrap -->
   <script src="../../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- jQuery UI -->
+  <script src="../../../plugins/jquery-ui/jquery-ui.min.js"></script>
   <!-- AdminLTE App -->
   <script src="../../../dist/js/adminlte.min.js"></script>
+  <!-- fullCalendar 2.2.5 -->
+  <script src="../../../plugins/fullcalendar/main.js"></script>
+  <script src="../../../plugins/fullcalendar/locales-all.js"></script>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var initialLocaleCode = 'pt-br';
+      var localeSelectorEl = document.getElementById('locale-selector');
+      var calendarEl = document.getElementById('calendar');
+
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+          left: 'prevYear,prev,next,nextYear today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+        },
+        themeSystem: 'bootstrap',
+        initialDate: '2020-09-12',
+        locale: initialLocaleCode,
+        buttonIcons: true, // show the prev/next text
+        weekNumbers: false,
+        navLinks: true, // can click day/week names to navigate views
+
+        selectMirror: true, // function selected click day
+        select: function(arg) {
+          var title = prompt('Event Title:');
+          if (title) {
+            calendar.addEvent({
+              title: title,
+              start: arg.start,
+              end: arg.end,
+              allDay: arg.allDay
+            })
+          }
+          calendar.unselect()
+        },
+        eventClick: function(arg) {
+          if (confirm('Are you sure you want to delete this event?')) {
+            arg.event.remove()
+          }
+        },
+
+        editable: true,
+        dayMaxEvents: true, // allow "more" link when too many events
+
+        displayEventTime: false, // don't show the time column in list view
+
+        selectable: true,
+        businessHours: true,
+
+        events: [{
+            title: 'Conference',
+            start: '2020-09-11',
+            end: '2020-09-13'
+          },
+          {
+            title: 'Meeting',
+            start: '2020-09-12T10:30:00',
+            end: '2020-09-12T12:30:00'
+          },
+          {
+            title: 'Lunch',
+            start: '2020-09-12T12:00:00'
+          },
+          {
+            title: 'Meeting',
+            start: '2020-09-12T14:30:00'
+          },
+          {
+            title: 'Happy Hour',
+            start: '2020-09-12T17:30:00'
+          },
+          {
+            title: 'Click for Google',
+            url: 'http://google.com/',
+            start: '2020-09-28'
+          }
+        ]
+      });
+
+      calendar.render();
+
+      // build the locale selector's options
+      calendar.getAvailableLocaleCodes().forEach(function(localeCode) {
+        var optionEl = document.createElement('option');
+        optionEl.value = localeCode;
+        optionEl.selected = localeCode == initialLocaleCode;
+        optionEl.innerText = localeCode;
+        localeSelectorEl.appendChild(optionEl);
+      });
+
+      // when the selected option changes, dynamically change the calendar option
+      localeSelectorEl.addEventListener('change', function() {
+        if (this.value) {
+          calendar.setOption('locale', this.value);
+        }
+      });
+
+    });
+  </script>
 </body>
 
 </html>
